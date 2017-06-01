70c8088714973ca24c523d085db996758a88237d
Willem Toorop <willem@nlnetlabs.nl>
2017-06-01 09:28:49 +0200

    Verify the qname qtype too

--- test-vectors/verify-chain.c
+++ test-vectors/verify-chain.c
@@ -9,29 +9,42 @@ int main(int argc, const char **argv)
 	getdns_return_t r = GETDNS_RETURN_GENERIC_ERROR;
 	FILE *chain_file = NULL;
 	getdns_list *chain = NULL;
-	getdns_list *support = NULL;
 	getdns_list *to_validate = NULL;
+	getdns_list *support = NULL;
+	getdns_list *answer = NULL;
+	getdns_dict *request = NULL;
 	size_t chain_len, i;
 	getdns_return_t dnssec_status;
+	char qname_str[1024];
+	getdns_bindata *qname;
 
-	if (argc != 3)
-		fprintf(stderr, "usage: %s <trust anchor file> <chain file>\n"
-		              , argv[0]);
+	if (argc != 5)
+		fprintf(stderr, "usage: %s <trust anchor file> <chain file>"
+				" <domain name> <port>\n", argv[0]);
 
-	else if (!(to_validate = getdns_list_create()))
+	else if (snprintf( qname_str, sizeof(qname_str), "_%s._tcp.%s."
+	                 , argv[4], argv[3]) < 0)
+		fprintf(stderr, "Problem with snprinf\n");
+
+	else if (!(support = getdns_list_create()) ||
+	    !(answer = getdns_list_create()) ||
+	    !(to_validate = getdns_list_create()))
 		fprintf(stderr, "Error creating list\n");
 
-	else if (!(support = getdns_list_create()))
-		fprintf(stderr, "Error creating list\n");
+	else if (!(request = getdns_dict_create()))
+		fprintf(stderr, "Error creating dict\n");
 
 	else if (!(ta_file = fopen(argv[1], "r")))
 		perror("Error opening trust anchor file");
 
-	else if ((r = getdns_fp2rr_list(ta_file, &tas, NULL, 0))) 
-		fprintf(stderr, "Error reading trust anchor file");
-
 	else if (!(chain_file = fopen(argv[2], "r")))
 		perror("Error opening chain file");
+
+	else if ((r = getdns_str2bindata(qname_str, &qname)))
+		fprintf(stderr, "Cannot make qname from \"%s\"", qname_str);
+
+	else if ((r = getdns_fp2rr_list(ta_file, &tas, NULL, 0))) 
+		fprintf(stderr, "Error reading trust anchor file");
 
 	else if ((r = getdns_fp2rr_list(chain_file, &chain, NULL, 0))) 
 		fprintf(stderr, "Error reading chain file");
@@ -57,7 +70,7 @@ int main(int argc, const char **argv)
 			break;
 		}
 		append = (rr_type == GETDNS_RRTYPE_DNSKEY ||
-		          rr_type == GETDNS_RRTYPE_DS) ? support : to_validate;
+		          rr_type == GETDNS_RRTYPE_DS) ? support : answer;
 	
 		if ((r = getdns_list_get_length(append, &a)) ||
 		    (r = getdns_list_set_dict(append, a, rr))) {
@@ -65,13 +78,24 @@ int main(int argc, const char **argv)
 			break;
 		}
 	}
-	if (r == GETDNS_RETURN_GOOD &&
-	    (dnssec_status = getdns_validate_dnssec2(to_validate, support, tas,
-	     1496233729, 0) != GETDNS_DNSSEC_SECURE)) {
+	if (r != GETDNS_RETURN_GOOD) ; /* pass */
+	else if ((r = getdns_dict_set_bindata(request, "/question/qname", qname)) ||
+	    (r = getdns_dict_set_int(request, "/question/qtype", GETDNS_RRTYPE_TLSA)) ||
+	    (r = getdns_dict_set_int(request, "/question/qclass", GETDNS_RRCLASS_IN)))
+		fprintf(stderr, "Error setting question");
+
+	else if ((r = getdns_dict_set_list(request, "answer", answer)))
+		fprintf(stderr, "Error setting answer");
+
+	else if ((r = getdns_list_set_dict(to_validate, 0, request)))
+		fprintf(stderr, "Error setting request");
+
+	else if ((dnssec_status = getdns_validate_dnssec2(to_validate,
+	    support, tas, 1496233729, 0) != GETDNS_DNSSEC_SECURE)) {
 		fprintf(stderr, "Chain did not validate");
 		r = dnssec_status;
 
-	} else if (r == GETDNS_RETURN_GOOD) {
+	} else {
 		uint8_t buf[8192], *ptr = buf;
 		int buf_sz = sizeof(buf);
 
